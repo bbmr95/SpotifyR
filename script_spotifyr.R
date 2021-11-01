a6eebc3c56e2f78ffc8071cf89974e9e70c1c364
bbmr95 <49081500+bbmr95@users.noreply.github.com>
2021-11-01 18:59:21 -0500

    Update script_spotifyr.R

--- script_spotifyr.R
+++ script_spotifyr.R
@@ -8,6 +8,13 @@ Sys.setenv(SPOTIFY_CLIENT_ID = '3f8718cf44964c5e88b5058c828e4626')
 Sys.setenv(SPOTIFY_CLIENT_SECRET = '89a1e4e2566944668bb90fed660bc5ab')
 access_token <- get_spotify_access_token()
 
+
+vector_date = c(
+format(Sys.Date(),'%d'),
+format(Sys.Date(),'%m'),
+format(Sys.Date(),'%y')
+)
+
 fn_id_artists = function(vec) {
   pre_base_artist_0 = c()
   for (x in vec) {
@@ -66,55 +73,52 @@ fn_id_tracks = function(vec) {
 
 
 # creación de listas
-lista_artistas = fn_id_artists('Anti Rudo Records')
+listas = list(fn_id_artists('Kids on Coffee'),fn_id_albums(fn_id_artists('Kids on Coffee')),fn_id_tracks(fn_id_albums(fn_id_artists('Kids on Coffee'))))
 
-lista_albums = fn_id_albums(lista_artistas)
-
-lista_tracks = fn_id_tracks(lista_albums)
+lista_bases = list(data.frame(),data.frame(),data.frame())
 
 
 # Base de artistas
-base_artists = data.frame()
-for (x in 1:length(lista_artistas)) {
-base_artists = 
-  rbind(base_artists,
-  get_artists(lista_artistas[[x]]) %>%
+for (x in 1:length(listas[[1]])) {
+lista_bases[[1]] = 
+  rbind(lista_bases[[1]],
+  get_artists(listas[[1]][[x]]) %>%
   tidyr::unnest(cols = 'images') %>% 
   group_by(id, name, popularity, type, followers.total, genres) %>% 
   filter(height == max(height)) %>%
   mutate(fec = Sys.Date(),genres = paste(genres)) %>%
-  rename(name_artists = name, followers = followers.total, popularity_artists = popularity, id_artists = id) %>%
-  select(id_artists, name_artists, popularity_artists, type, followers, genres, url, fec)
+  rename(name_artists = name, followers = followers.total, popularity_artists = popularity, id_artists = id, type_artists = type) %>%
+  select(id_artists, name_artists, popularity_artists, type_artists, followers, genres, url, fec)
   )
 }
 
+
 # Base de albums
-base_albums = data.frame()
-for (x in 1:length(lista_albums)) {
-base_albums = 
-  rbind(base_albums,
-  get_albums(lista_albums[[x]]) %>%
+
+for (x in 1:length(listas[[2]])) {
+lista_bases[[2]] = 
+  rbind(lista_bases[[2]],
+  get_albums(listas[[2]][[x]]) %>%
   tidyr::unnest(cols = 'images') %>% 
   group_by(id, name, type, popularity, release_date, available_markets, total_tracks, label, genres) %>% 
   filter(height == max(height)) %>%
   mutate(fec = Sys.Date(),available_markets = paste(available_markets), genres = paste(genres)) %>%
-  rename(name_albums = name, popularity_albums = popularity, type_album = type, id_album = id) %>%
-  select(id_album, name_albums, type_album, popularity_albums, release_date, available_markets, total_tracks, label, genres, url, fec)
+  rename(name_album = name, popularity_albums = popularity, type_album = type, id_album = id) %>%
+  select(id_album, name_album, type_album, popularity_albums, release_date, available_markets, total_tracks, label, genres, url, fec)
   )
 }
 
 # Base de canciones
-base_tracks = data.frame()
-for (x in 1:length(lista_tracks)) {
-base_tracks = 
-  rbind(base_tracks,
+for (x in 1:length(listas[[3]])) {
+lista_bases[[3]] = 
+  rbind(lista_bases[[3]],
   merge(
-  x = get_tracks(lista_tracks[[x]]) %>% 
+  x = get_tracks(listas[[3]][[x]]) %>% 
     tidyr::unnest(cols = artists, name_sep = '.') %>%
     mutate(fec = Sys.Date(),available_markets = paste(available_markets)) %>%
     rename(id_track = id1, name_track = name1, id_artist = id, artist.name = name, popularity_track = popularity, id_album = album.id) %>%
     select(id_track, name_track, duration_ms, popularity_track, id_album, id_artist, track_number, available_markets, explicit, is_local, fec),
-  y = get_track_audio_features(lista_tracks[[x]]) %>%
+  y = get_track_audio_features(listas[[3]][[x]]) %>%
     rename(id_track = id) %>%
     select(id_track, danceability, energy, loudness, speechiness, acousticness, instrumentalness, liveness, valence, 
            tempo, time_signature, key, mode),
@@ -122,3 +126,7 @@ base_tracks =
   all.x = T)
   )
 }
+
+remove(fn_id_albums,fn_id_artists,fn_id_tracks,x)
+
+save.image(paste("bases_spotify/kids_on_coffee/20",vector_date[3],"_",vector_date[2],"_",vector_date[1],".RData",sep=''))
